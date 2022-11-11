13130171fe037169b67d4688f5f408bb82c5ed25
Kawabuchi Kota(川渕 皓太)A <kawabuchiA216@future.co.jp>
2022-11-11 16:36:29 +0900

    development

---
+++ examples/as.sql
@@ -0,0 +1,16 @@
+
+--test
+/* test */
+select 
+--test
+a, --test
+b  --test
+/* multi */
+--test
+from tab1 /*ttesttestesttesetetssss */
+ AS t1 --test
+ , tab2 AS t2, tabtabtabtabtab AS long  --test
+where  --test
+        1  = 2 + 1 --test
+AND ttesttestesttesetetssss = t1 --test tests
+--test
---
+++ test.js
@@ -0,0 +1,16 @@
+const { runfmt } = require("./index.js");
+let target = `
+SELECT
+      Identifier as id, --ID 
+student_name          --              学生名
+FROM japanese_student_table  AS JPN_STD --日本人学生
+,       SUBJECT_TABLE AS SBJ  --科目
+WHERE JPN_STD.sportId = (SELECT           sportId   FROM
+    Sport
+                         WHERE           Sport.sportname 
+    = 'baseball'                  )   -- 野球をしている生徒
+    AND 
+JPN_STD.ID  = SBJ.ID            AND SBJ.grade   > 
+        /*grade*/50     --成績が50点以上    
+`;
+console.log(runfmt(target));
